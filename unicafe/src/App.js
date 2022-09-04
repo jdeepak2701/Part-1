45be863eb8a67a80e65887507786e92999561da6
jdeepak2701 <111353521+jdeepak2701@users.noreply.github.com>
2022-09-04 18:41:35 +0530

    Update App.js

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,19 +1,19 @@
 import { useState } from "react";
-
-const Button = ({ handleClick, text }) => (
+const Button = ({ handleClick, text }) =>
+(
   <button onClick={handleClick}>{text}</button>
 );
-
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => 
+{
   const all = good + neutral + bad;
   const average = all > 0 ? roundToTwo((good - bad) / all) : 0;
   const positive = all > 0 ? roundToTwo((good / all) * 100) : 0;
-
-  if (all === 0) {
+  if (all === 0) 
+  {
     return <div>No feedback given</div>;
   }
-
-  return (
+  return 
+  (
     <table>
       <tbody>
         <StatisticLine text="good" value={good} />
@@ -26,24 +26,22 @@ const Statistics = ({ good, neutral, bad }) => {
     </table>
   );
 };
-
-const StatisticLine = ({ text, value }) => (
+const StatisticLine = ({ text, value }) => 
+(
   <tr>
     <td>{text}</td>
     <td>{value}</td>
   </tr>
 );
-
-// Round to two decimal places
 const roundToTwo = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
-
-const App = () => {
+const App = () => 
+{
   // Save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  return (
+  return
+ (
     <div>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
@@ -54,5 +52,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
